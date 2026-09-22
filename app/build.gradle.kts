e10777b4c09120499d673a2cbe692cf4a749cf2d
LeonardoPiolla <leonardo.piolla@hotmail.com>
2026-09-22 17:00:55 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,15 @@ android {
     }
     buildFeatures { compose = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.4" }
+    
+    // --> CÓDIGO NOVO ADICIONADO AQUI <--
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
