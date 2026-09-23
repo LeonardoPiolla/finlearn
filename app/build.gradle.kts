809ee36c25a173f44a9f85b19396392cd722e2df
LeonardoPiolla <leonardo.piolla@hotmail.com>
2026-09-23 12:40:18 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,15 +14,11 @@ android {
     }
     buildFeatures { compose = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.4" }
-    
-    // --> CÓDIGO NOVO ADICIONADO AQUI <--
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
@@ -31,4 +27,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    
+    // Biblioteca oficial para armazenamento local offline
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
