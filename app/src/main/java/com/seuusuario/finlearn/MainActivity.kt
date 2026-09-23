ab257c6f1f5800b3f1feaf3c003651a84db2e079
LeonardoPiolla <leonardo.piolla@hotmail.com>
2026-09-22 22:12:36 -0300

    Update MainActivity.kt

--- app/src/main/java/com/seuusuario/finlearn/MainActivity.kt
+++ app/src/main/java/com/seuusuario/finlearn/MainActivity.kt
@@ -3,13 +3,30 @@ package com.seuusuario.finlearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Chama a nova tela gamificada
-            HomeTrailScreen()
+            var currentScreen by remember { mutableStateOf("home") }
+            var totalXp by remember { mutableStateOf(0) }
+            var streakDays by remember { mutableStateOf(1) }
+
+            when (currentScreen) {
+                "home" -> HomeTrailScreen(
+                    currentXp = totalXp,
+                    streakDays = streakDays,
+                    onStartLesson = { currentScreen = "lesson" }
+                )
+                "lesson" -> LessonScreen(
+                    onFinishLesson = { xpGained ->
+                        totalXp += xpGained
+                        currentScreen = "home"
+                    },
+                    onBack = { currentScreen = "home" }
+                )
+            }
         }
     }
 }
