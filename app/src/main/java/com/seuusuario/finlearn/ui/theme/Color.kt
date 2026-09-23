9bf9df611b854612ffbb2da853474e44fe2489d7
LeonardoPiolla <leonardo.piolla@hotmail.com>
2026-09-22 21:58:08 -0300

    Create Color.kt

---
+++ app/src/main/java/com/seuusuario/finlearn/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.seuusuario.finlearn.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF1CB0F6) // Azul principal lúdico
+val CorrectGreen = Color(0xFF58CC02) // Verde para acertos
+val StreakFlame = Color(0xFFFF9600) // Laranja para ofensiva (foguinho)
+val BackgroundLight = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF4B4B4B)
